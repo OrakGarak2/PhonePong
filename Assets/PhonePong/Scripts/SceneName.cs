377dc0eae71a4c24a85f6197cedb5c99ae8747fa
Junsang <kimnora070623@gmail.com>
2025-06-11 01:53:20 +0900

    추가 | 스플래시 화면 및 메인 메뉴 구성

---
+++ Assets/PhonePong/Scripts/SceneName.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace PhonePong
+{
+    public static class SceneName
+    {
+        public const string SplashScene = "00_Splash";
+        public const string MainScene = "01_MainMenu";
+    }
+}
+
