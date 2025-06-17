593040b4d2bdd58e602f2e7b34085bed8df5d7c7
OrakGarak2 <888seonghun888@gmail.com>
2025-06-17 19:31:22 +0900

    추가 | 드로우 퐁

--- Assets/PhonePong/Scripts/InGame/Draw Pong/DrawLine.cs
+++ Assets/PhonePong/Scripts/InGame/Draw Pong/DrawLine.cs
@@ -5,55 +5,117 @@ using System.Collections.Generic;
 
 // Unity
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DrawLine : MonoBehaviour
 {
-    [SerializeField] private GameObject linePrefab;
-    [SerializeField] private GameObject currentLine;
+    [Header("라인 라켓")]
+    [SerializeField] private GameObject lineRacketPrefab;
+    [SerializeField] private GameObject currentLineRacket;
 
     [SerializeField] private LineRenderer lineRenderer;
     [SerializeField] private EdgeCollider2D edgeCollider2D;
     [SerializeField] private List<Vector2> pointPosList;
 
+    [SerializeField] private int layerBitMask;
+
+    private const float nextLinePointMinDistance = 0.15f;
+
+    [Header("게이지")]
+    [SerializeField] private Image lineGauge;
+    private const float lineGaugeMaxAmount = 1f;
+    [SerializeField] private float lineGaugeAmount = lineGaugeMaxAmount;
+    [SerializeField] private float lineGaugeIncreaseRate;
+    [SerializeField] private float lineGaugeDecreaseRate;
+
+
+    private void Start()
+    {
+        layerBitMask = 1 << gameObject.layer;
+    }
+
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && CheckPointInZone(out Vector2 pointPos))
         {
-            CreateLine();
+            CreateLine(pointPos);
         }
         else if (Input.GetMouseButton(0))
         {
-            Vector2 tempPointPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            if (Vector2.Distance(tempPointPos, pointPosList[pointPosList.Count - 1]) > .1f)
+            if (currentLineRacket == null) { return; }
+
+            if (CheckPointInZone(out Vector2 nextPointPos))
             {
-                UpdateLine(tempPointPos);
+                if (lineGaugeAmount > 0f && Vector2.Distance(nextPointPos, pointPosList[pointPosList.Count - 1]) > nextLinePointMinDistance)
+                {
+                    UpdateLine(nextPointPos);
+                }
             }
+            else
+            {
+                DeleteLine();
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            DeleteLine();
+        }
+        else if (lineGaugeAmount != lineGaugeMaxAmount)
+        {
+            UpdateLineGauge(lineGaugeAmount + lineGaugeIncreaseRate);
         }
     }
 
-    private void CreateLine()
+    private bool CheckPointInZone(out Vector2 pointPos)
     {
-        currentLine = Instantiate(linePrefab, Vector2.zero, Quaternion.identity);
-        lineRenderer = currentLine.GetComponent<LineRenderer>();
-        edgeCollider2D = currentLine.GetComponent<EdgeCollider2D>();
+        pointPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Debug.Log($"Physics2D.OverlapPoint(pointPos, thisLayerMask): {Physics2D.OverlapPoint(pointPos, layerBitMask) != null}");
+        Collider2D hit = Physics2D.OverlapPoint(pointPos, layerBitMask);
+
+        return hit != null ? hit.gameObject == gameObject : false;
+    }
+
+    private void CreateLine(Vector2 pointPos)
+    {
+        currentLineRacket = Instantiate(lineRacketPrefab, Vector2.zero, Quaternion.identity);
+        lineRenderer = currentLineRacket.GetComponent<LineRenderer>();
+        edgeCollider2D = currentLineRacket.GetComponent<EdgeCollider2D>();
 
         pointPosList.Clear();
-        pointPosList.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-        pointPosList.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        pointPosList.Add(pointPos);
 
+        lineRenderer.positionCount = 1;
         lineRenderer.SetPosition(0, pointPosList[0]);
-        lineRenderer.SetPosition(1, pointPosList[1]);
 
         edgeCollider2D.points = pointPosList.ToArray();
+
+        edgeCollider2D.enabled = true;
     }
 
     private void UpdateLine(Vector2 newPointPos)
     {
+        UpdateLineGauge(lineGaugeAmount - lineGaugeDecreaseRate);
+
         pointPosList.Add(newPointPos);
 
         lineRenderer.positionCount++;
         lineRenderer.SetPosition(lineRenderer.positionCount - 1, newPointPos);
 
         edgeCollider2D.points = pointPosList.ToArray();
+    }
+
+    private void DeleteLine()
+    {
+        if (currentLineRacket != null)
+        {
+            Destroy(currentLineRacket);
+        }
+    }
+
+    private void UpdateLineGauge(float newAmount)
+    {
+        lineGaugeAmount = Mathf.Clamp(newAmount, 0f, lineGaugeMaxAmount);
+        lineGauge.fillAmount = lineGaugeAmount;
     }
 }
