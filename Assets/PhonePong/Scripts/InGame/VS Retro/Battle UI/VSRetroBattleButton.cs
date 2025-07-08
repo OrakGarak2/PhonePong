8519265c9e6a1d4e604e6609aae0d699061e5fe2
OrakGarak2 <888seonghun888@gmail.com>
2025-07-08 20:43:08 +0900

    추가 | 툴팁, 전투씬의 메뉴 버튼 UI

--- Assets/PhonePong/Scripts/InGame/VS Retro/Battle UI/VSRetroBattleButton.cs
+++ Assets/PhonePong/Scripts/InGame/VS Retro/Battle UI/VSRetroBattleButton.cs
@@ -7,15 +7,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+// PhonePong
+using PhonePong.VSRetro.Tooltip;
+
 namespace PhonePong.VSRetro.Battle
 {
     public class VSRetroBattleButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
         [SerializeField] protected bool interactable;
-        [SerializeField] protected float holdDuration;
-        protected const float tooltipHoldDuration = 1f;
 
+        [Header("툴팁")]
         protected Coroutine currentHoldTimeCoroutine;
+        protected TooltipUser tooltipUser;
+
+        protected const float tooltipHoldDuration = 1f;
+        [SerializeField] protected bool onTooltip;
+        [SerializeField] protected Vector2 tooltipPosOffset = new Vector2(-475f, 0f);
+        [SerializeField] protected string tooltipContentName;
+        [SerializeField] protected string tooltipContentDesc;
+
+        protected virtual void Start()
+        {
+            SetInteractable(true);
+        }
+
+        public virtual void SetInteractable(bool value) => interactable = value;
 
         public virtual void OnPointerDown(PointerEventData eventData)
         {
@@ -32,39 +48,61 @@ namespace PhonePong.VSRetro.Battle
             {
                 StopCoroutine(currentHoldTimeCoroutine);
                 currentHoldTimeCoroutine = null;
-
-                if (holdDuration < tooltipHoldDuration)
-                {
-                    SelectThis();
-                }
-                else
-                {
-                    ShowTooltip();
-                }
             }
 
-            holdDuration = 0f;
+            if (!onTooltip)
+            {
+                SelectThis();
+            }
+            else
+            {
+                HideTooltip();
+            }
         }
-
-        public virtual void SetInteractable(bool value) => interactable = value;
 
         protected virtual IEnumerator HoldTimeCoroutine()
         {
+            float holdDuration = tooltipHoldDuration;
+
             while (interactable)
             {
-                holdDuration += Time.deltaTime;
+                holdDuration -= Time.deltaTime;
+
+                if (holdDuration < 0)
+                {
+                    ShowTooltip();
+                    break;
+                }
+
                 yield return null;
             }
         }
 
         protected virtual void SelectThis()
         {
-            
+
         }
 
         protected virtual void ShowTooltip()
         {
-            
+            if (tooltipUser == null)
+            {
+                SetTooltipUser();
+            }
+
+            onTooltip = true;
+            tooltipUser.ShowTooltip();
+        }
+
+        protected virtual void HideTooltip()
+        {
+            onTooltip = false;
+            tooltipUser.HideTooltip();
+        }
+
+        protected virtual void SetTooltipUser()
+        {
+            tooltipUser = new TooltipUser((Vector2)transform.position + tooltipPosOffset, tooltipContentName, tooltipContentDesc);
         }
     }
 }
