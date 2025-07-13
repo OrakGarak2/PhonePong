7c489c0522416aae14890923e9a197fd166dd50b
OrakGarak2 <888seonghun888@gmail.com>
2025-07-13 17:36:35 +0900

    수정 | 에어바운스가 거리 비례로 튕기게 함, 더 극적으로 튕기도록 함

--- Assets/PhonePong/Scripts/InGame/Ability Pong/Ability/AirBounceAbility.cs
+++ Assets/PhonePong/Scripts/InGame/Ability Pong/Ability/AirBounceAbility.cs
@@ -8,8 +8,8 @@ using UnityEngine;
 
 public class AirBounceAbility : Ability
 {
-    [SerializeField] float minAirBounceWaitTime = 1f;
-    [SerializeField] float maxAirBounceWaitTime = 1.5f;
+    [SerializeField] float minAirBounceDistance = 8f;
+    [SerializeField] float maxAirBounceDistance = 10f;
 
     public override void Excute(AbilityPaddle paddle)
     {
@@ -23,8 +23,24 @@ public class AirBounceAbility : Ability
 
     private IEnumerator CoroutineAirBounce(Rigidbody2D rb2D)
     {
-        yield return new WaitForSeconds(UnityEngine.Random.Range(minAirBounceWaitTime, maxAirBounceWaitTime));
+        Vector2 beforePos = rb2D.position;
+        float bounceDistance = UnityEngine.Random.Range(minAirBounceDistance, maxAirBounceDistance);
+        float movingDistance = 0f;
 
-        rb2D.linearVelocityY = -rb2D.linearVelocityY;
+        while (movingDistance < bounceDistance)
+        {
+            movingDistance += Vector2.Distance(rb2D.position, beforePos);
+            Debug.Log($"movingDistance: {movingDistance}, bounceDistance: {bounceDistance}");
+            beforePos = rb2D.position;
+            yield return null;
+        }
+
+        float minusSignVelocityY = Mathf.Sign(-rb2D.linearVelocityY);
+        float maxBouncedVelocityY = Mathf.Abs(rb2D.linearVelocityX) + Mathf.Abs(rb2D.linearVelocityY);
+
+        // velocity의 y가 x 이상이도록 설정(적어도 45도로 날아감.)
+        Vector2 direction = new Vector2(rb2D.linearVelocityX, UnityEngine.Random.Range(Mathf.Abs(rb2D.linearVelocityX), maxBouncedVelocityY) * minusSignVelocityY).normalized;
+
+        rb2D.linearVelocity = rb2D.linearVelocity.magnitude * direction;
     }
 }
